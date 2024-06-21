3251c9ffff3a1724b20cf5fb1d379742d2d8750e
Nisar Ali <mrnisarali110@gmail.com>
2024-06-21 20:55:41 +0500

    Routes+header+footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
 import SignupForm from './components/SignupForm'
 import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Wrapper from './components/Wrapper';
+import SignIn from './SignIn';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Wrapper />,
+    children: [
+      {
+        index: true,
+        element: <SignupForm />,
+      },
+    ],
+  },
+  {
+    path: "/signin",
+    element: <Wrapper />,
+    children: [
+      {
+        index: true,
+        element: <SignIn />,
+      },
+    ],
+  },
 
-const App = () => {
-  return (
-    <div>
-      <SignupForm/>
-    </div>
-  )
+
+
+
+
+
+
+
+
+])
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
