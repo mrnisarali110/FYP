2a17240b3c5bc5caaec8ae07e0e639dfb30194b9
mrnisarali110 <mrnisarali110@gmail.com>
2024-09-15 18:02:25 +0500

    routes set

--- backend/routers/auth.routers.js
+++ backend/routers/auth.routers.js
@@ -6,8 +6,6 @@ const authRoutes = express.Router();
 // Define the registration route
 authRoutes.post('/register', Register);
 authRoutes.post('/login', Login);
-authRoutes.post('/forgot-password-link', ForgotPasswordLink);
-authRoutes.post('/forgot-password', ForgotPassword);
 authRoutes.post('/reset-password', ResetPassword);
 
 module.exports = authRoutes;
